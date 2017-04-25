4f508e81f4aa4851202c4c3bf6b62004682028a8
Cat Wylie <cjwylie@mymail.mines.edu>
2017-04-25 14:29:39 -0600

    Added implementation for middle panel (black background)

--- src/gui/MiddlePanel.java
+++ src/gui/MiddlePanel.java
@@ -1,7 +1,17 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 public class MiddlePanel extends JPanel{
-
+	public MiddlePanel(){
+		//set grid
+		setLayout(new GridLayout(0,0));
+		//set color
+		setBackground(new Color(3,3,54));
+		//mess with this
+		setPreferredSize(getSize());
+	}
 }
