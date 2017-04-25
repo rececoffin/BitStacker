37908a187b9adcc4833081aa9f6dd8ddaea5678b
Cat Wylie <cjwylie@mymail.mines.edu>
2017-04-25 14:24:17 -0600

    Created random color generation function

--- src/game/Block.java
+++ src/game/Block.java
@@ -1,11 +1,14 @@
 package game;
 
+import java.awt.Color;
 import java.util.Random;
 
 public class Block {
 	private int bit;
+	public Color blockColor;
 	public Block() {
 		bit = 0;
+		setRandomColor();
 	}
 
 	//generate a random bit value for a single block
@@ -23,6 +26,51 @@ public class Block {
 
 	public void setBit(int b) {
 		this.bit = b;
+	}
+	
+	public void setRandomColor(){
+		Random r = new Random();
+		int n = r.nextInt(7);
+		switch (n){
+		case 0: 
+			//purple
+			blockColor = new Color(153,0,255);
+			break;
+		case 1: 
+			//pink
+			blockColor = new Color(255,3,214);
+			break;
+		case 2: 
+			//cyan
+			blockColor = new Color(0,255,255);
+			break;
+		case 3: 
+			//blue cyan weirdness
+			blockColor = new Color(0,255,188);
+			break;
+		case 4: 
+			//orange
+			blockColor = new Color(255,162,0);
+			break;
+		case 5: 
+			//another blue
+			blockColor = new Color(0,145,255);
+			break;
+		case 6: 
+			//green
+			blockColor = new Color(0,255,85);
+			break;
+//		case 7: 
+//			//
+//			blockColor = new Color(0,0,0);
+//			break;
+		default: 
+			//just in case
+			blockColor = Color.WHITE;
+		}
+	}
+	public Color getBlockColor() {
+		return blockColor;
 	}
 
 }
